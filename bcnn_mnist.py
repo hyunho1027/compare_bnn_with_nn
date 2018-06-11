4e5306de44b07b7feac777e858cdf63ff1eecb0c
hyunho1027 <hyunho1027@khu.ac.kr>
2018-06-11 21:20:15 +0900

    hyperparams modified.

--- bcnn_mnist.py
+++ bcnn_mnist.py
@@ -70,7 +70,7 @@ inference = ed.KLqp({w1: qw1, w2: qw2, w3: qw3, w4: qw4, w5: qw5,
                      b4: qb4, b5: qb5 }, data={y:y_ph})
 
 # Initialse the infernce variables
-inference.initialize(n_iter=10000, n_print=100, scale={y: float(mnist.train.num_examples) / N})
+inference.initialize(n_iter=5000, n_print=100, scale={y: float(mnist.train.num_examples) / N})
 
 # We will use an interactive session.
 sess = tf.InteractiveSession()
